03d06b13c2875b6242e560064c5b06f258eb0d5a
omarion3698 <omaribinbakarivic@gmail.com>
2020-07-24 22:47:54 +0300

    Edit styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function validate() {
   var genders = document.getElementsByName("gender");
   if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
@@ -31,7 +30,7 @@ function validate() {
   }
 }
 
-function calculateDayValue(){
+function calculateDayValue() {
   year = document.getElementById("year").value;
   CC = parseInt(year.substring(0,2));
   YY = parseInt(year.substring(2,4));
@@ -42,60 +41,60 @@ function calculateDayValue(){
   return (Math.floor(d));
 }
 
-function getGender(){
+function getGender() {
   var genders = document.getElementsByName("gender");
-  if(genders[0].checked == true){
+  if(genders[0].checked == true) {
     var gender = "male";
   }
-  else if(genders[1].checked == true){
+  else if(genders[1].checked == true) {
     var gender = "female";
   }
   else{
     return false;
   }
-  switch(gender){
+  switch(gender) {
     case "male":
-      if (dayValue == 1){
+      if (dayValue == 1) {
         alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
       }
-      else if(dayValue == 2){
+      else if(dayValue == 2) {
         alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
       }
-      else if(dayValue == 3){
+      else if(dayValue == 3) {
         alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
       }
-      else if(dayValue == 4){
+      else if(dayValue == 4) {
         alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
       }
-      else if(dayValue == 5){
+      else if(dayValue == 5) {
         alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
       }
-      else if(dayValue == 6){
+      else if(dayValue == 6) {
         alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
       }
-      else if(dayValue == -0){
+      else if(dayValue == -0) {
         alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
       }
     break;
     case "female":
-      if (dayValue == 1){
+      if (dayValue == 1) {
         alert("You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0]+"!");
       }
-      else if(dayValue == 2){
+      else if(dayValue == 2) {
         alert("You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1]+"!");
       }
-      else if(dayValue == 3){
+      else if(dayValue == 3) {
         alert("You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2]+"!");
       }
-      else if(dayValue == 4){
+      else if(dayValue == 4) {
         alert("You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3]+"!");
       }
-      else if(dayValue == 5){
+      else if(dayValue == 5) {
         alert("You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4]+"!");
       }
-      else if(dayValue == 6){
+      else if(dayValue == 6) {
         alert("You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5]+"!");
-      }else if(dayValue == -0){
+      }else if(dayValue == -0) {
         alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
       }
     break
@@ -103,7 +102,7 @@ function getGender(){
           
   }
 }
-function findName(){
+function findName() {
   dayValue = calculateDayValue();
   getGender();
 }
