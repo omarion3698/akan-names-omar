7e9541365871212ec40eedbff7e1fad5482962fb
omarion3698 <omaribinbakarivic@gmail.com>
2020-07-25 12:27:48 +0300

    Latest Update

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,8 @@ function validate() {
 
 function calculateDayValue() {
   year = document.getElementById("year").value;
-  CC = parseInt(year.substring(0,2));
-  YY = parseInt(year.substring(2,4));
+  CC = parseInt(year.slice(0,1));
+  YY = parseInt(year.slice(2,3));
   MM = parseInt(document.getElementById("month").value);
   DD = parseInt(document.getElementById("date").value);
   d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
